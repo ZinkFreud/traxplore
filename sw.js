9e84e6ab9409a063cd8d26adbdaf8162985f397e
ZinkFreud <121689004+ZinkFreud@users.noreply.github.com>
2026-09-08 15:00:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,12 +12,12 @@
    yayinda degistirmek gerekiyor, yoksa telefon eski dosyalari tutar.
    ===================================================================== */
 
-const SURUM  = "traxplore-20260908i";
+const SURUM  = "traxplore-20260908j";
 const KABUK  = [
   "./",
   "./index.html",
-  "./style.css?v=20260908i",
-  "./script.js?v=20260908i",
+  "./style.css?v=20260908j",
+  "./script.js?v=20260908j",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
