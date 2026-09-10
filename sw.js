7b1890eb4666170e5e4f80c147d198d5bb13b153
ZinkFreud <121689004+ZinkFreud@users.noreply.github.com>
2026-09-10 21:05:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,17 +12,16 @@
    yayinda degistirmek gerekiyor, yoksa telefon eski dosyalari tutar.
    ===================================================================== */
 
-const SURUM  = "traxplore-20260910l";
+const SURUM  = "traxplore-20260909g";
 const KABUK  = [
   "./",
   "./index.html",
-  "./style.css?v=20260910l",
-  "./script.js?v=20260910l",
+  "./style.css?v=20260909g",
+  "./script.js?v=20260909g",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
-  "./apple-touch-icon.png",
-  "./onizleme.png?v=1"
+  "./apple-touch-icon.png"
 ];
 /* Bunlar baska sunucudan geliyor; biri inmezse kurulum bozulmasin diye
    tek tek ve hatayi yutarak ekliyoruz. */
@@ -32,16 +31,13 @@ const DISARIDAN = [
 ];
 
 self.addEventListener("install", function (olay) {
-  /* addAll ATOMIK: listedeki tek bir dosya inmezse (404, kesinti, yeni
-     surum daha yayilmamis) kurulum tamamen basarisiz oluyor, yeni
-     servis iscisi hic devreye girmiyor ve telefon eski dosyalarla
-     kaliyor. Tek tek ekleyip hatayi yutuyoruz: eksik kalan dosya
-     internetten gelir, kurulum yine de tamamlanir. */
   olay.waitUntil(
     caches.open(SURUM).then(function (kap) {
-      return Promise.all(KABUK.concat(DISARIDAN).map(function (a) {
-        return kap.add(a).catch(function () {});
-      }));
+      return kap.addAll(KABUK).then(function () {
+        return Promise.all(DISARIDAN.map(function (a) {
+          return kap.add(a).catch(function () {});
+        }));
+      });
     }).then(function () { return self.skipWaiting(); })
   );
 });
